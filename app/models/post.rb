a415320f72c0839adeb879019e8ae9430693b654
Alexandr Egorov <wwwean@gmail.com>
2012-06-03 23:14:10 +0400

    Added posts and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  attr_accessible :title, :body, :author
   validates :title, :presence => true
   validates :body, :presence => true, :length => { :minimum => 10 }
+  has_many :comments
 end
