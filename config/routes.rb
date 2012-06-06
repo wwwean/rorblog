3427ec5421498c7938919f6fb6b0570f4941a0fe
Alexandr Egorov <wwwean@gmail.com>
2012-06-06 23:08:05 +0400

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rorblog::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :blog
+  resources :users
 
   # Sample resource route with options:
   #   resources :products do
