45b67cae6036086ff243fe5eecc804c060cb0580
Alexandr Egorov <wwwean@gmail.com>
2012-06-09 19:08:49 +0400

    Sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rorblog::Application.routes.draw do
 
+  resources :sessions, :only => [:new, :create, :destroy]
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   root :to => 'pages#home'
 
   resources :posts do
